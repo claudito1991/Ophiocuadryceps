646be3da0a484991ec2f354765d878d85529820d
Mau Delgaudio <mauro_delgaudio@outlook.com>
2023-02-04 07:02:21 -0300

    Bullet self destroy timer

--- Assets/FungiBullet.cs
+++ Assets/FungiBullet.cs
@@ -1,7 +1,18 @@
+using System.Collections;
 using UnityEngine;
 
-public class FungiBullet : MonoBehaviour
-{
+public class FungiBullet : MonoBehaviour {
+    [SerializeField] private float m_LifeSpan = 5;
+
+    private void Start() {
+        StartCoroutine(AutoDestroy());
+    }
+
+    private IEnumerator AutoDestroy() {
+        yield return new WaitForSeconds(m_LifeSpan);
+        Destroy(gameObject);
+    }
+
     private void OnTriggerEnter2D(Collider2D col) {
         if (!col.CompareTag("Ant")) return;
         
