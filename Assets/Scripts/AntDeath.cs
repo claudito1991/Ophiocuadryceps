a135f21cd375f52c41dfab21777b493a2c19f7e6
Claudio Oulego <claudio.oulego@gmail.com>
2023-02-04 19:27:33 -0300

    Win and lose text placeholder and condition working as expected

--- Assets/Scripts/AntDeath.cs
+++ Assets/Scripts/AntDeath.cs
@@ -6,17 +6,27 @@ public class AntDeath : MonoBehaviour {
     [SerializeField] private float m_MinLifespan;
     [SerializeField] private float m_MaxLifespan;
 
+    private Coroutine coroutine;
+
     void Start() {
         ant = GetComponent<Ant>();  
     }
 
     void OnEnable()
     {
-       StartCoroutine(TimerXD());
+       coroutine = StartCoroutine(TimerXD());
     }
     IEnumerator TimerXD() {
         yield return new WaitForSeconds(Random.Range(m_MinLifespan, m_MaxLifespan));
         Debug.Log("muerte");
         Destroy(gameObject);
+    }
+
+    void OnDisable()
+    {
+        if(coroutine !=null)
+        {
+            StopCoroutine(coroutine);
+        }
     }
 }
