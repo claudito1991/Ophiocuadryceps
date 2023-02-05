fbde5dd50ce35c6be64b016c40ad460b007d1a77
Mau Delgaudio <mauro_delgaudio@outlook.com>
2023-02-05 12:23:09 -0300

    Ant movement code thinned down

--- Assets/Scripts/AntMovement.cs
+++ Assets/Scripts/AntMovement.cs
@@ -4,20 +4,33 @@ public class AntMovement : MonoBehaviour {
     [SerializeField] private float m_TurnSpeed = 30;
     [SerializeField] private float m_FollowRangeY = 1.5f;
     [SerializeField] private float m_MinSpeed = 0.8f;
-    [SerializeField] private float m_MaxSpeed =2.0f;
-
-    [SerializeField] private float speedModifier;
+    [SerializeField] private float m_MaxSpeed = 2.0f;
 
     private Ant ant;
     private Rigidbody2D rb2d;
 
-    [SerializeField]private bool antInSapBool;
+    private bool isSlowedBySap;
 
     private void Awake() {
         ant = GetComponent<Ant>();
         rb2d = GetComponent<Rigidbody2D>();
-        antInSapBool = false;
-        speedModifier=1f;
+        isSlowedBySap = false;
+    }
+
+    private float GetSpeedBlend() {
+        if (!ant.IsPossessed) {
+            return 0.0f;
+        }
+
+        var leaderAnt = FungiMind.GetLeaderAnt();
+
+        if (leaderAnt == ant) {
+            return 0.5f;
+        }
+
+        var deltaToLeader = leaderAnt.GetPosition() - ant.GetPosition();
+        var normalizedDelta = Mathf.InverseLerp(-1, 1, deltaToLeader.y / m_FollowRangeY);
+        return Mathf.Clamp01(normalizedDelta);
     }
 
     private void Update() {
@@ -25,41 +38,17 @@ public class AntMovement : MonoBehaviour {
             rb2d.rotation = Mathf.MoveTowardsAngle(rb2d.rotation, 90, Time.deltaTime * m_TurnSpeed);
         }
 
-        float speedBlend;
-        var leaderAnt = FungiMind.GetLeaderAnt();
+        var targetSpeed = Mathf.Lerp(m_MinSpeed, m_MaxSpeed, GetSpeedBlend());
+        var speedMultiplier = isSlowedBySap ? 0.5f : 1f;
 
-        if (leaderAnt == ant || !ant.IsPossessed) {
-            speedBlend = 0.5f;
-        }
-        else {
-            var deltaToLeader = leaderAnt.GetPosition() - ant.GetPosition();
-            var normalizedDelta = Mathf.InverseLerp(-1, 1, deltaToLeader.y / m_FollowRangeY);
-            speedBlend = Mathf.Clamp01(normalizedDelta);
-        }
-
-        if(antInSapBool)
-        {
-            speedModifier = 0.5f;
-        }
-
-        else
-        {
-            speedModifier = 1f;
-        }
-
-        var speedLerp = Mathf.Lerp(m_MinSpeed, m_MaxSpeed, speedBlend);
-        rb2d.velocity = transform.right * speedLerp * speedModifier;
-
-       
+        rb2d.velocity = transform.right * targetSpeed * speedMultiplier;
     }
 
-    void OnDisable()
-    {
+    private void OnDisable() {
         Destroy(rb2d);
     }
 
-    public bool AntInSap(bool state)
-    {
-        return antInSapBool = state;
+    public bool AntInSap(bool state) {
+        return isSlowedBySap = state;
     }
 }
