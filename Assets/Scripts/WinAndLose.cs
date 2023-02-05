635f9d0766ae3b8aa475e3137c5368d145c755e5
Mau Delgaudio <mauro_delgaudio@outlook.com>
2023-02-05 11:09:32 -0300

    win trigger responds to newly possessed ants already inside the trigger

--- Assets/Scripts/WinAndLose.cs
+++ Assets/Scripts/WinAndLose.cs
@@ -25,7 +25,7 @@ public class WinAndLose : MonoBehaviour {
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
+    private void OnTriggerStay2D(Collider2D other) {
         if (!other.CompareTag("Ant") || !other.GetComponent<Ant>().IsPossessed) {
             return;
         }
