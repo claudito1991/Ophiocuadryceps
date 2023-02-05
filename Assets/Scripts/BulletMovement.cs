de3854b9df86ea8003603748036084ee977922a5
Mau Delgaudio <mauro_delgaudio@outlook.com>
2023-02-04 22:13:43 -0300

    new bullet death behaviour

--- Assets/Scripts/BulletMovement.cs
+++ Assets/Scripts/BulletMovement.cs
@@ -4,13 +4,18 @@ public class BulletMovement : MonoBehaviour
 {
     [SerializeField] private float m_Speed = 20f;
     private Rigidbody2D rb2d;
+    private FungiBullet bullet;
 
     private void Awake() {
         rb2d = GetComponent<Rigidbody2D>();
+        bullet = GetComponent<FungiBullet>();
     }
 
     private void Update()
     {
-        rb2d.velocity = transform.right * m_Speed;
+        if(bullet.IsDead)
+            rb2d.velocity = Vector2.zero;
+        else
+            rb2d.velocity = transform.right * m_Speed;
     }
 }
--- Assets/Scripts/FungiBullet.cs
+++ Assets/Scripts/FungiBullet.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 public class FungiBullet : MonoBehaviour {
     [SerializeField] private float m_LifeSpan = 5;
 
+    private Rigidbody2D rb2d;
+
+    public bool IsDead { get; private set; }
+
+    private void Awake() {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
     private void Start() {
         StartCoroutine(AutoDestroy());
     }
@@ -14,6 +22,8 @@ public class FungiBullet : MonoBehaviour {
     }
 
     private void OnTriggerEnter2D(Collider2D col) {
+        if (IsDead) return;
+        
         if (!col.CompareTag("Ant")) return;
         
         var ant = col.GetComponent<Ant>();
@@ -21,6 +31,19 @@ public class FungiBullet : MonoBehaviour {
         if (ant.IsPossessed) return;
         
         ant.OnBulletHit();
-        Destroy(gameObject);
+        Kill();
+    }
+
+    private void Kill() {
+        if (IsDead) return;
+        
+        IsDead = true;
+
+        foreach (var ps in GetComponentsInChildren<ParticleSystem>()) {
+            var emissionModule = ps.emission;
+            emissionModule.enabled = false;
+        }
+        
+        Destroy(gameObject, 0.6f);
     }
 }
